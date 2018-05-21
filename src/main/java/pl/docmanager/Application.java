d46e8d82b30cc68b95b5e61d2de55dc1d84dbd93
Sergiusz <deephans@o2.pl>
2018-05-21 20:50:45 +0200

    added postgresql hibernate configuration

--- src/main/java/pl/docmanager/Application.java
+++ src/main/java/pl/docmanager/Application.java
@@ -1,12 +1,12 @@
-package pl.docmanager.web;
-
-import java.util.Arrays;
+package pl.docmanager;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
@@ -25,5 +25,4 @@ public class Application {
             }
         };
     }
-
 }
