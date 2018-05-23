e364788b5f7e8e8f984ccea465961d5e2a9a86fb
Sergiusz <deephans@o2.pl>
2018-05-23 17:19:45 +0200

    added UserSettings domain class

---
+++ src/main/java/pl/docmanager/domain/user/UserSettings.java
@@ -0,0 +1,72 @@
+package pl.docmanager.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usersettings")
+public class UserSettings {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "userid", nullable = false)
+    private User user;
+
+    @Column(name = "name", length = 256, nullable = false)
+    private String name;
+
+    @Column(name = "value", length = 1024, nullable = false)
+    private String value;
+
+    @Column(name = "domain", length = 2048)
+    private String domain;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+}
