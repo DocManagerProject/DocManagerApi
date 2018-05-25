456e6d6a6958aab359656fc888e78fa104be0616
Sergiusz <deephans@o2.pl>
2018-05-25 18:07:34 +0200

    added basic api authentication filter

---
+++ src/main/java/pl/docmanager/web/security/ApiAuthenticationFilter.java
@@ -0,0 +1,57 @@
+package pl.docmanager.web.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class ApiAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private static final Logger log = LogManager.getLogger(ApiAuthenticationFilter.class);
+    private AuthenticationManager authenticationManager;
+
+    public static final String TEMPORARY_SECRET = "secret";
+
+    public ApiAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+            UserPasswordCredentials credentials = new ObjectMapper()
+                    .readValue(request.getInputStream(), UserPasswordCredentials.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            credentials.getUsername(), credentials.getPassword()));
+        } catch (IOException e) {
+            log.error("Error while authenticating user", e);
+        }
+        return null;
+    }
+
+    public void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                         FilterChain chain, Authentication authResult) {
+        String apiToken = Jwts.builder()
+                .setSubject(((User) authResult.getPrincipal()).getUsername())
+                .setExpiration(new Date(System.currentTimeMillis() + 10000))
+                .signWith(SignatureAlgorithm.HS512, TEMPORARY_SECRET.getBytes())
+                .compact();
+        response.addHeader("apiToken", apiToken);
+    }
+}
