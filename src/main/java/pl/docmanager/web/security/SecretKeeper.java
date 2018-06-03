c9ad72a849d1cf65b399d133b15a75c4c332fed6
Sergiusz <deephans@o2.pl>
2018-06-03 18:35:57 +0200

    changed SecretKeeper to singleton class

--- src/main/java/pl/docmanager/web/security/SecretKeeper.java
+++ src/main/java/pl/docmanager/web/security/SecretKeeper.java
@@ -1,7 +1,18 @@
 package pl.docmanager.web.security;
 
 public class SecretKeeper {
-    public static String getSecret() {
+    private static SecretKeeper instance;
+
+    private SecretKeeper() { }
+
+    public synchronized static SecretKeeper getInstance() {
+        if (instance == null) {
+            instance = new SecretKeeper();
+        }
+        return instance;
+    }
+
+    public String getSecret() {
         return "SecretKeyToGenJWTs";
     }
 }
